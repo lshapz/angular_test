7b4203315a0aa023fe6ee71c4fccc271f39b3561
Laura Shapiro <laurashapiro01@gmail.com>
2019-02-26 12:28:37 -0500

    brief attempt to use @vue/vue-web-component-wrapper, realized it's not ready for prime time re: props

--- src/app/vue-chart/vue-chart.component.ts
+++ src/app/vue-chart/vue-chart.component.ts
@@ -3,26 +3,27 @@ import { Component, OnInit, Input } from '@angular/core';
 import 'jscatalyst/dist/jscatalyst.min.css';
 import Vue from 'vue';
 import Vuex from 'vuex';
-import vueCustomElement from 'vue-custom-element';
+import wrap from '@vue/web-component-wrapper';
+// import vueCustomElement from 'vue-custom-element';
 import {D3PieChart, ThemePlugin} from 'jscatalyst';
 Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {}
 });
 Vue.use(ThemePlugin, {store, custom: false});
-Vue.use(vueCustomElement);
+// Vue.use(vueCustomElement);
 
-Vue.customElement('vue-chart-vue', {
-  store: store,
-  props: ['data'],
-  components: {D3PieChart},
-  computed: {
-    pieData() {
-      return this.data.map(item => {
-        return {label: item.name, value: item.value};
-      });
-    }
-  },
+const VueComponent =  {
+  // store: store,
+  // props: ['data'],
+  // components: {D3PieChart},
+  // computed: {
+  //   pieData() {
+  //     return this.data.map(item => {
+  //       return {label: item.name, value: item.value};
+  //     });
+  //   }
+  // },
   render(createElement) {
     return createElement(
       'div', {
@@ -30,19 +31,22 @@ Vue.customElement('vue-chart-vue', {
           'style': 'height: 500px; width: 500px; margin: 0 auto;'
         }
       }, [
-        createElement(
-          D3PieChart, {
-            props: {
-              dataModel: this.pieData
-            },
-          }
+        createElement('h1', 'your mom'
+          // D3PieChart, {
+          //   props: {
+          //     dataModel: this.pieData
+          //   },
+          // }
         )
       ]
     );
   }
 
-});
+};
 
+const CustomElement = wrap(Vue, VueComponent);
+
+window.customElements.define('vue-chart-vue', CustomElement);
 
 @Component({
   selector: 'app-vue-chart',
